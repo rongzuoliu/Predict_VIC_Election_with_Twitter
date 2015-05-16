8e836eb8062686b4f04ee9a67527de6142c12855
Rongzuo Liu <rongzuoliu@gmail.com>
2015-05-16 17:32:14 +1000

    final results (very important!!!)

---
+++ SumUp/electDataSum.js
@@ -0,0 +1,5 @@
+var electDataSum = {
+"type": "Sum of Predicted Election Results ",
+"parties": {
+{"Nationals": {"neu_rate": 0, "neg_rate": 0, "total_pos": 893, "total_neu": 938, "total": 2097, "total_neg": 266, "pos_rate": 0}, "Liberal": {"neu_rate": 0, "neg_rate": 0, "total_pos": 7779, "total_neu": 10557, "total": 22771, "total_neg": 4435, "pos_rate": 0}, "Greens": {"neu_rate": 0, "neg_rate": 0, "total_pos": 6272, "total_neu": 7414, "total": 16248, "total_neg": 2562, "pos_rate": 0}, "Labor": {"neu_rate": 0, "neg_rate": 0, "total_pos": 18727, "total_neu": 21452, "total": 47864, "total_neg": 7685, "pos_rate": 0}}
+}}
