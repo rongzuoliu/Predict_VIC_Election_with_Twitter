0cbd21897c2d06bfd5d2271288d37ebb8aaff8de
Rongzuo Liu <rongzuoliu@gmail.com>
2015-05-29 02:10:47 +1000

    data

---
+++ Source/PolitClassification.py
@@ -0,0 +1,18 @@
+__author__ = 'rongzuoliu'
+
+PARTIES = ['Labor', 'Liberal', 'Greens', 'Nationals']
+
+LEADERS = ['Daniel Andrews', 'DanielAndrewsMP',
+           'Napthine', 'denisvnapthine',
+           'Greg Barber', 'GregMLC']
+
+# LABOR = ['Labor', 'Daniel Andrews', 'DanielAndrewsMP']
+# LIBERAL = ['Liberal', 'Napthine', 'denisvnapthine']
+# GREENS = ['Greens', 'Greg Barber', 'GregMLC']
+# NATIONALS = ['Nationals']
+
+PARTYANDLEADER = {'Labor': ['Labor', 'Daniel Andrews', 'DanielAndrewsMP'],
+      'Liberal': ['Liberal', 'Napthine', 'denisvnapthine'],
+      'Greens': ['Greens', 'Greg Barber', 'GregMLC'],
+      'Nationals': ['Nationals']
+    }
