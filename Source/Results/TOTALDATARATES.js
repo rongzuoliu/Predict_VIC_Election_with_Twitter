3ec88dcbacf756823381906ba91c505805a144b3
Rongzuo Liu <rongzuoliu@gmail.com>
2015-06-08 02:44:39 +1000

    part of predicted results

---
+++ Source/Results/TOTALDATARATES.js
@@ -0,0 +1,9 @@
+var totalDataRates = {
+'type': 'Sum of the Total Counts and Rates for Every Party',
+'total count': 79927,
+'parties': {
+'Nationals': {'neu_rate': 0.008933151500744429, 'neg_rate': 0.0026273975002189497, 'total_pos': 730, 'total_neu': 714, 'total': 1654, 'total_neg': 210, 'pos_rate': 0.009133334167427778},
+'Liberal': {'neu_rate': 0.14090357451174196, 'neg_rate': 0.05926658075493888, 'total_pos': 8262, 'total_neu': 11262, 'total': 24261, 'total_neg': 4737, 'pos_rate': 0.10336932450861411},
+'Greens': {'neu_rate': 0.07325434458943786, 'neg_rate': 0.027037171418919766, 'total_pos': 5231, 'total_neu': 5855, 'total': 13247, 'total_neg': 2161, 'pos_rate': 0.06544722058878727},
+'Labor': {'neu_rate': 0.22352897018529408, 'neg_rate': 0.08252530434021044, 'total_pos': 16303, 'total_neu': 17866, 'total': 40765, 'total_neg': 6596, 'pos_rate': 0.20397362593366447}
+}};
