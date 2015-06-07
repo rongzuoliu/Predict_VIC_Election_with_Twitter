c4a78b40e0d1fdf4beac22a7f625e5a14f14c8ac
Rongzuo Liu <rongzuoliu@gmail.com>
2015-06-08 02:04:29 +1000

    modified web data files

---
+++ web/data/ELECTDATASUMBYTIME.js
@@ -0,0 +1,10 @@
+var electDataSumByTime = {
+'type': 'Counts of Predicted Election Results ',
+'totalCount': '102497',
+'timeLines': {
+'2014': {'Liberal': {'neg': 3794, 'total': 19108, 'neu': 9038, 'pos': 6276}, 'Nationals': {'neg': 169, 'total': 1507, 'neu': 694, 'pos': 644}, 'Greens': {'neg': 1384, 'total': 8340, 'neu': 3732, 'pos': 3224}, 'Labor': {'neg': 4390, 'total': 28312, 'neu': 12951, 'pos': 10971}},
+'2011': {'Liberal': {'neg': 163, 'total': 844, 'neu': 375, 'pos': 306}, 'Nationals': {'neg': 20, 'total': 98, 'neu': 41, 'pos': 37}, 'Greens': {'neg': 252, 'total': 1242, 'neu': 549, 'pos': 441}, 'Labor': {'neg': 503, 'total': 2423, 'neu': 961, 'pos': 959}},
+'2010': {'Liberal': {'neg': 9, 'total': 56, 'neu': 27, 'pos': 20}, 'Nationals': {'neg': 2, 'total': 5, 'neu': 1, 'pos': 2}, 'Greens': {'neg': 20, 'total': 84, 'neu': 27, 'pos': 37}, 'Labor': {'neg': 21, 'total': 100, 'neu': 40, 'pos': 39}},
+'2013': {'Liberal': {'neg': 1551, 'total': 8451, 'neu': 3737, 'pos': 3163}, 'Nationals': {'neg': 73, 'total': 747, 'neu': 314, 'pos': 360}, 'Greens': {'neg': 754, 'total': 4903, 'neu': 2176, 'pos': 1973}, 'Labor': {'neg': 2941, 'total': 16646, 'neu': 6816, 'pos': 6889}},
+'2012': {'Liberal': {'neg': 344, 'total': 1717, 'neu': 763, 'pos': 610}, 'Nationals': {'neg': 24, 'total': 205, 'neu': 89, 'pos': 92}, 'Greens': {'neg': 414, 'total': 2770, 'neu': 1283, 'pos': 1073}, 'Labor': {'neg': 936, 'total': 4939, 'neu': 2084, 'pos': 1919}}
+}};
